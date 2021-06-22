997ccc2045db111092af18340233c0513f00a2dd
Michael Bláha <michael.blaha@eluvia.com>
2021-06-23 00:03:34 +0200

    update version

--- setup.py
+++ setup.py
@@ -5,15 +5,13 @@ with open("README.md", "r") as fh:
 
 setup(
     name='clockify-api-client',
-    version='0.0.2',
+    version='0.0.3',
     author="Michael Bláha",
     author_email="michael.blaha@eluvia.com",
     description="Simple python API client for clockify. Inspired by https://pypi.org/project/clockify/ library.",
     long_description=long_description,
-    py_modules=['clockify_api_client'],
     long_description_content_type="text/markdown",
     url="https://github.com/eluvia-com/clockify-api-aclient",
-    package_dir={'': 'clockify_api_client'},
     install_requires=['requests', 'factory_boy'],
     classifiers=[
         "Programming Language :: Python :: 3",
