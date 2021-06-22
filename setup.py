acfd6b472c6600b89c36b0030780ba601ab445b6
Michael Bláha <michael.blaha@eluvia.com>
2021-06-22 12:08:36 +0200

    Add manifest, add license, update setup

--- setup.py
+++ setup.py
@@ -1,27 +1,22 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name='clockify-api-client',  # Required
-    version='0.5.0',  # Required
+    name='clockify-api-client',
+    version='0.0.1',
     author="Michael Bláha",
     author_email="michael.blaha@eluvia.com",
-    description="Simple python API client for clockify.",
+    description="Simple python API client for clockify. Inspired by https://pypi.org/project/clockify/ library.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://gitlab.spkapps.eu/libraries/python/clockify-api-client",
-    packages=find_packages(),
-
-    install_requires=[
-        'requests', 'factory_boy'
-    ],
-
+    url="https://github.com/eluvia-com/clockify-api-aclient",
+    package_dir={'': 'clockify_api_client'},
+    install_requires=['requests', 'factory_boy'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: MIT License",
         "Operating System :: OS Independent",
     ],
-    setup_requires=['wheel'],
 )
